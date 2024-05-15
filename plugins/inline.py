ee47f25a104d0c72268bb77b31c54c9aafe1b5fa
ALNWALKERTG <166110788+ALNWALKERTG@users.noreply.github.com>
2024-05-15 16:17:03 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -7,7 +7,7 @@ from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
-cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
+cache_time = 86400 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
 
 async def inline_users(query: InlineQuery):
     if AUTH_USERS:
